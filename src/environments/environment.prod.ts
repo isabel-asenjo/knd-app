59a0977fac003c152443f8bbc65149b460da21c4
RoccoMadonna <rocco.madonna@correo.unimet.edu.ve>
2020-11-18 00:29:33 -0400

    firebase en admin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAFPKk_KElLybWiQ_n3xWHTRSgCWWIb93U",
+    authDomain: "knd-app.firebaseapp.com",
+    databaseURL: "https://knd-app.firebaseio.com",
+    projectId: "knd-app",
+    storageBucket: "knd-app.appspot.com",
+    messagingSenderId: "311116829935",
+    appId: "1:311116829935:web:e4dca8e7cf4d5c8967771a",
+    measurementId: "G-QN2RHH78G5"
+  },
 };
