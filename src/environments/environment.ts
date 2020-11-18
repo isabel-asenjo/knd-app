af2fc60606c3c84ab8e397f8a141fc92bd22e0ee
RoccoMadonna <rocco.madonna@correo.unimet.edu.ve>
2020-11-17 23:46:26 -0400

    first firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAFPKk_KElLybWiQ_n3xWHTRSgCWWIb93U",
+    authDomain: "knd-app.firebaseapp.com",
+    databaseURL: "https://knd-app.firebaseio.com",
+    projectId: "knd-app",
+    storageBucket: "knd-app.appspot.com",
+    messagingSenderId: "311116829935",
+    appId: "1:311116829935:web:e4dca8e7cf4d5c8967771a",
+    measurementId: "G-QN2RHH78G5"
+  },
 };
 
 /*
