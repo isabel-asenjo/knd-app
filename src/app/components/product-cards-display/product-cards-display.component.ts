ec677e024f76944ec65d0121a4cd0534bdd28cdf
Isabel <isabel.asenjo@correo.unimet.edu.ve>
2020-12-09 23:26:55 -0400

    meh

--- src/app/components/product-cards-display/product-cards-display.component.ts
+++ src/app/components/product-cards-display/product-cards-display.component.ts
@@ -13,63 +13,13 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./product-cards-display.component.scss']
 })
 export class ProductCardsDisplayComponent implements OnInit {
-  products: Array<Product> = [];
-  singleProd: Product = null;
-  productId: string;
 
 
   constructor(private productService : ProductsService, private router: Router, private route: ActivatedRoute) { }
-  
-  filterProd = '';
-  p: number = 1;
+
 
   ngOnInit(): void {
-    this.getProducts();
   }
-
-  
-  getProducts(): void {
-    this.productService.getAllProducts().subscribe((items) => {
-      this.products = items.map(
-        (item) =>
-          ({
-            ...item.payload.doc.data(),
-            $key: item.payload.doc['id'],
-          } as Product)
-      )
-    });
-  }
-
-
-  showDeets(prodId: string): void{
-    this.productService.getProduct(prodId).subscribe(item =>{
-      this.singleProd = item.payload.data();
-      $key: item.payload["id"],
-      
-      this.router.navigate(['products/:this.prodId']);
-      return this.singleProd;
-    })
-    
-  }
-
-
-
-  getUrlParams(): void {
-    this.route.paramMap.subscribe(params => {
-      this.productId = params.get('prodId');
-
-      if(this.productId) {
-        this.productService.getProduct(this.productId).subscribe(item =>{
-          this.singleProd = {
-            ...item.payload.data(),
-            $key: item.payload["id"],
-          };
-              
-        })
-      }
-    })
-  }
-
 }
 
 
