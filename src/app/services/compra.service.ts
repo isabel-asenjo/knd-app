475177ffb296912b765e210428ed6d6e2ad0ebc2
Isabel <isabel.asenjo@correo.unimet.edu.ve>
2020-12-10 02:49:52 -0400

    omg my first model+service thingy

---
+++ src/app/services/compra.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Action, AngularFirestore, AngularFirestoreCollection, DocumentChangeAction, DocumentSnapshot } from '@angular/fire/firestore';
+import { database } from 'firebase';
+import { Observable } from 'rxjs';
+import { Compra } from '../models/compra';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompraService {
+  private compraCollection: AngularFirestoreCollection<Compra>;
+  constructor(private db: AngularFirestore) {
+    this.compraCollection = this.db.collection<Compra>('compra');
+   }
+
+
+
+  getAllCompras(): Observable<DocumentChangeAction<Compra>[]>{
+    return this.compraCollection.snapshotChanges();
+  }
+
+  getCompra(compraId: string): Observable<Action<DocumentSnapshot<Compra>>>{
+    return this.compraCollection.doc<Compra>(compraId).snapshotChanges();
+  }
+  
+  createProduct(newCompra: Compra): Promise<any> {
+    return this.compraCollection.add(newCompra);
+  }
+
+  updateProduct(data: Compra, docId: string): Promise<void> {
+    return this.compraCollection.doc<Compra>(docId).update(data);
+  }
+
+  deleteProduct(docId: string): Promise<void>{
+    return this.compraCollection.doc<Compra>(docId).delete();
+  }
+
+}
+
